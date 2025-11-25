59abf8012041922616980cbea09966d0254cf0f2
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2025-11-25 11:49:57 -0600

    signup is complete

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,10 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env
 
 export default function Login() {
     const router = useRouter();
+    const [user, setUser] = useState<string>("")
     const [password, setPassword] = useState<string>("");
+    const [error, setError] = useState<string>("");
+
 
     useEffect(() => {
         async function getSession() {
@@ -25,24 +28,50 @@ export default function Login() {
 
     async function handleLogin() {
         try {
-            const { data } = await supabase.auth.signInWithPassword({
-                email: 'jake.c.eckhardt@gmail.com',
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email: user,
                 password: password,
             });
+
+            if (error) {
+                setError(error.message);
+                return;
+            }
 
             if (data) {
                 router.push('/');
             }
+
         } catch (error) {
-            console.error('Error signing up:', error);
+            console.error('Error logging in:', error);
         };
     };
 
     return (
         <div className={styles.loginContainer}>
-            <h1>Prove it.</h1>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleLogin}>Login</button>
+            <div className={styles.innerContainer}>
+                <h1>Login</h1>
+                {error.length > 0 && (
+                    <div className={styles.errorContainer}>
+                        <p>{error}</p>
+                    </div>
+                )}
+                <div className={styles.inputContainer}>
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="text" value={user} onChange={(e) => setUser(e.target.value)} />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button onClick={handleLogin}>Login</button>
+                <div className={styles.orDivider}>
+                    <span/>
+                    <p>or</p>
+                    <span/>
+                </div>
+                <a href="/signup" className={styles.signup}>Signup</a>
+            </div>
         </div>
     )
 };
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { useRouter } from 'next/navigation';
+import styles from "./Navigation.module.scss";
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_PUBLISHABLE_KEY!)
+
+export default function Navigation() {
+    const router = useRouter();
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+    function handleUserButton() {
+        if (userLoggedIn) {
+            supabase.auth.signOut();
+            router.push('/login');
+            return;
+        }
+        router.push('/login');
+    };
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setUserLoggedIn(session === null ? false : true);
+        });
+    
+        const { data: listener } = supabase.auth.onAuthStateChange(
+            (_event, session) => {
+                setUserLoggedIn(session === null ? false : true);
+            }
+        );
+    
+        return () => listener.subscription.unsubscribe();  
+    }, []);
+
+    return (
+        <div className={styles.navigationContainer}>
+            <h1>Task Timer</h1>
+            <div className={styles.navButtons}>
+                <button
+                    onClick={handleUserButton}
+                >
+                    {userLoggedIn ? "Logout" : "Login"}
+                </button>
+            </div>
+        </div>
+    )
+};
